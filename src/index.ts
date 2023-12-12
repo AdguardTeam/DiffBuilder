f28ff05d9bca44696690f533702625af2af60a54
Dmitriy Seregin <d.seregin@adguard.com>
2023-12-12 19:14:47 +0300

    better code

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,2 @@
-import { DiffUpdater } from './diff-updater';
-import { DiffBuilder } from './diff-builder';
-
-export {
-    DiffUpdater,
-    DiffBuilder,
-};
+export { DiffUpdater } from './diff-updater';
+export { DiffBuilder } from './diff-builder';
